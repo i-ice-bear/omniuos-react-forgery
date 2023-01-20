a7dc0735ceae789350e223430eda1c0031b3c7c2
Anshu Naagar <108215633+i-ice-bear@users.noreply.github.com>
2023-01-20 14:41:54 +0000

    Prractiec module initialized

--- textutils/src/source/content/Practice.js
+++ textutils/src/source/content/Practice.js
@@ -3,14 +3,20 @@ import PropTypes from "prop-types";
 import React, { Component } from "react";
 
 export default class Practice extends Component {
-  static propTypes = { name: PropTypes.string };
-  something() {
+    static propTypes = { name: PropTypes.string };
+    something() {
     console.log("called outside")
   }
-  constructor(props) {
+  componentDidMount() {
+      setInterval(()=>{
+          const date = new Date();
+          const changeDate = date  + 1;
+          this.date = changeDate 
+        }, 1000)
+    }
+    constructor(props) {
     super(props);
     this.state = {
-
     };
   }
 
@@ -19,6 +25,7 @@ export default class Practice extends Component {
       <>
         <div className="container">
           <h1>I'm {this.props.name}</h1>
+          <h5>Date : {this.date}</h5>
           <Button onPress={this.something}>
             Click to console something
           </Button>

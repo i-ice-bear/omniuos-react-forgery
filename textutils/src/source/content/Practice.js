9019b33ed5bb08992518b8d8975ec13712bf9d92
Anshu Naagar <108215633+i-ice-bear@users.noreply.github.com>
2023-01-21 16:11:15 +0000

    Typescript templatee commit

--- textutils/src/source/content/Practice.js
+++ textutils/src/source/content/Practice.js
@@ -2,29 +2,28 @@ import { Button } from "@nextui-org/react";
 import PropTypes from "prop-types";
 import React, { Component } from "react";
 
-export default class Practice extends Component {
+export default class Practice extends Component  {
     static propTypes = { name: PropTypes.string };
+  date: any;
     something() {
     console.log("called outside")
   }
   componentDidMount() {
       setInterval(()=>{
           const date = new Date();
-          const changeDate = date  + 1;
+          const changeDate = date;
           this.date = changeDate 
         }, 1000)
     }
     constructor(props) {
     super(props);
-    this.state = {
-    };
   }
 
   render() {
     return (
       <>
         <div className="container">
-          <h1>I'm {this.props.name}</h1>
+          {/* TODO: <h1>I'm {this.props.name}</h1> */}
           <h5>Date : {this.date}</h5>
           <Button onPress={this.something}>
             Click to console something

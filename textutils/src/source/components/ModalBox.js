4a7322edcd7e3e351dc2a74b9911d26285456183
Anshu Naagar <108215633+i-ice-bear@users.noreply.github.com>
2023-01-26 12:19:14 +0000

    Modal re-modified

--- textutils/src/source/components/ModalBox.js
+++ textutils/src/source/components/ModalBox.js
@@ -1,42 +1,86 @@
 import React from "react";
-import { Modal, Button, Image, Text, Link } from "@nextui-org/react";
+import {
+  Modal,
+  Button,
+  Image,
+  Text,
+  Link,
+  Container,
+  Input,
+  Row,
+  Checkbox,
+  Textarea,
+  Spacer,
+} from "@nextui-org/react";
+import { FaBug } from "react-icons/fa";
 
 export default function ModalCallOutComponent(props) {
-  
   return (
     <div>
-      <Modal noPadding open={props.visible} onClose={props.closeHandler}>
-        <Modal.Header
-          css={{ position: "absolute", zIndex: "$1", top: 5, right: 8 }}
-        >
-          <Text color="#363449">
-            Photo by{" "}
-            <Link
-              color
-              rel="noopener noreferrer"
-              target="_blank"
-              href="https://unsplash.com/@anniespratt"
-            >
-              Annie Spratt
-            </Link>{" "}
-            on{" "}
-            <Link
-              rel="noopener noreferrer"
-              target="_blank"
-              href="https://unsplash.com/s/visual/ef7937f3-0d44-43eb-b992-28050748f999?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
-            >
-              Unsplash
-            </Link>
+      <Modal
+        open={props.visible}
+        onClose={props.closeHandler}
+        css={{ padding: 10 }}
+        closeButton
+        blur
+        aria-labelledby="modal-title"
+      >
+        <Modal.Header>
+          <Text h5 id="modal-title">
+            Encounterd by any bug ? <FaBug />
           </Text>
         </Modal.Header>
+
         <Modal.Body>
-          <Image
-            showSkeleton
-            src="https://images.unsplash.com/photo-1459411552884-841db9b3cc2a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
-            width={400}
-            height={490}
+          <Spacer y={0.22} />
+          <Input
+            clearable
+            fullWidth
+            color="primary"
+            size="md"
+            labelPlaceholder="Your Email"
           />
+          <Spacer y={0.22} />
+
+          <Input
+            clearable
+            fullWidth
+            color="primary"
+            size="md"
+            labelPlaceholder="Your name"
+          />
+          <Spacer y={0.22} />
+
+          <Textarea labelPlaceholder="Describe your issue" />
+          <Row justify="space-between">
+            <Checkbox>
+              <Text h6 size={14} className="my-1">
+                Remember me
+              </Text>
+            </Checkbox>
+            <Text h6 size={14} className="my-1">
+              Forget password ?
+            </Text>
+          </Row>
+          <Text
+            size={14}
+            h6
+            className="text-dark fw-light"
+            style={{ lineHeight: 1.2 }}
+          >
+            Note : By clicking on submit, you're agree to our{" "}
+            <a href="#" style={{ textDecoration: "none" }}>
+              Terms and conditions
+            </a>{" "}
+            to agree for inspecting the issue, We will fix the bug as soon as
+            possible with an alert mail to the reporter
+          </Text>
         </Modal.Body>
+        <Modal.Footer>
+          <Button auto color="secondary" size="sm">
+            Submit
+          </Button>
+        </Modal.Footer>
       </Modal>
     </div>
   );
